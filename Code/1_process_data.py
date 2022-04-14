f050188b61e929d4932adbe58009461d2199c851
AlexH20 <97352766+AlexH20@users.noreply.github.com>
2022-04-14 12:27:08 +0200

    Update 1_process_data.py

--- Code/1_process_data.py
+++ Code/1_process_data.py
@@ -2,9 +2,50 @@ import csv
 import sys
 import pandas as pd
 import json
+import re
+import string
+from nltk.stem.porter import *
+stemmer = PorterStemmer()
+exclude = set(string.punctuation)
 
 #To max out field limit
 csv.field_size_limit(sys.maxsize)
+
+#### FUNCTIONS TO CLEAN PHRASES AND WORDS #### Frankel, Jennings and Lee (2021)
+
+stopwords = ['a','able','across','after','also','am','among','an','and','any','are','as','at','be','because','been','but','by','can','could','dear','did','do','does','either','else','ever','every','for','from','get','got','had','has','have','he','her','hers','him','his','how','however','i','if','in','into','is','it','its','just','let','like','likely','me','my','of','off','often','on','only','or','other','our','own','rather','said','say','says','she','should','since','so','some','than','that','the','their','them','then','there','these','they','this','tis','to','too','twas','us','wants','was','we','were','what','when','where','which','while','who','whom','why','will','with','would','yet','you','your']
+
+stopwords_dict={}
+for stopword in stopwords:
+    stopwords_dict[stopword]=0
+
+def fix_phrases(section):
+    section = re.sub('(\d+)\.(\d+)','\g<1>\g<2>',section) # Remove periods from numbers -- 4.55 --> 455
+    section = section.replace(".com", "com")
+    section = section.replace("-", " ")
+    section = section.replace('. .', '.')
+    section = section.replace('.', 'XXYYZZ1')
+    section = ''.join(ch for ch in section if ch not in exclude) #Delete all punctuation except periods
+    section = section.replace('XXYYZZ1', '.')
+    section = section.lower()
+    section = re.sub(' +',' ',section) #Remove multiple spaces
+    if section == '.': section = ''
+    return section
+
+def fixword(word):
+        word = word.replace('\n','')
+        if re.search('[0-9]',word) != None:
+            word = '00NUMBER00' # Replace numbers with 000NUMBER000
+        try:
+            test = stopwords_dict[word]
+            word = '_' # Replace stop words with _
+        except Exception:
+            donothing = 1
+        try:
+            word = stemmer.stem(word) # Stemp words
+        except Exception:
+            word = ''
+        return word
 
 #The following code extracts date, ticker and text from the json file in the NASDAQ data folder and creates a csv file with the three columns
 
@@ -60,6 +101,24 @@ with open(first_csv_path, newline='') as first_csv:
                 #If multiple tickers use first entry. IMPORTANT: adjust later so that file multiple tickers same text
                 news[1] = news[1].split(sep= ",")[0]
 
+                #Pre-processing text to reduce noise and prepare text data for word representation learning models. Part of the code is taken from Frankel, Jennings and Lee (2021)
+                news[2] = fix_phrases(news[2])
+                sentences = news[2].split('.')
+
+                for v,sentence in enumerate(sentences):
+
+                    sentences[v] = sentences[v].replace(".", "").strip()
+
+                    allwords = sentences[v].split(" ")
+
+                    for w, word in enumerate(allwords):
+                        allwords[w] = fixword(allwords[w])
+
+                    sentences[v] = " ".join(allwords)
+                    print(sentences[v])
+
+                news[2] = ".".join(sentences)
+
                 writer.writerow([news[0], news[1], news[2]])
 
         first_csv.close()
@@ -74,6 +133,7 @@ txtticker_path = "/Users/alexanderholzer/PycharmProjects/Thesis/Data/processed d
 with open(txtticker_path, "w") as f:
     for index, row in df.iterrows():
         f.write(str(row["Ticker"]) + "\n")
+
 
 
 
