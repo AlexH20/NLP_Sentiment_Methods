029465e2d4888716069144b67b16535a97dc763c
AlexH20 <97352766+AlexH20@users.noreply.github.com>
2022-04-15 19:56:31 +0200

    Update 1_process_data.py

--- Code/1_process_data.py
+++ Code/1_process_data.py
@@ -2,6 +2,7 @@ import csv
 import sys
 import pandas as pd
 import json
+import os
 import re
 import string
 from nltk.stem.porter import *
@@ -14,22 +15,23 @@ csv.field_size_limit(sys.maxsize)
 #The following code extracts date, ticker and text from the json file in the NASDAQ data folder and creates a csv file with the three columns
 
 json_file_path = "/Users/alexanderholzer/PycharmProjects/Thesis/Data/Nasdaq/NASDAQ_News.json"
-first_csv_path = "/Users/alexanderholzer/PycharmProjects/Thesis/Data/processed data/data_compact.csv"
+output_file_path = "/Users/alexanderholzer/PycharmProjects/Thesis/Data/processed data/"
 
 with open(json_file_path, 'r') as json_file:
-    with open(first_csv_path, 'w') as first_csv:
+    with open(output_file_path + "data_processed_interim.csv", 'w') as first_csv:
         writer = csv.writer(first_csv)
         writer.writerow(["Date", "Ticker", "Text"])
         for i, line in enumerate(json_file):
             data = json.loads(line)
 
-            try:
-                writer.writerow([data["article_time"]["$date"], data["symbols"], data["article_content"]])
-            except KeyError:
-                continue
+            if i % 10 == 0:
+                try:
+                    writer.writerow([data["article_time"]["$date"], data["symbols"], data["article_content"]])
+                except KeyError:
+                    continue
 
-            if i == 100:
-                break
+
+
 
         json_file.close()
         first_csv.close()
@@ -74,11 +76,9 @@ def fixword(word):
             word = ''
         return word
 
-csv_processed_path = "/Users/alexanderholzer/PycharmProjects/Thesis/Data/processed data/data_compact_processed.csv"
-
 #Open file to read and one to write
-with open(first_csv_path, newline='') as first_csv:
-    with open(csv_processed_path, "w") as csv_processed:
+with open(output_file_path + "data_processed_interim.csv", newline='') as first_csv:
+    with open(output_file_path + "data_processed.csv", "w") as csv_processed:
 
         nasdaq_news = csv.reader(first_csv)
 
@@ -88,23 +88,23 @@ with open(first_csv_path, newline='') as first_csv:
         # Skip header
         next(nasdaq_news, None)
 
-        for news in nasdaq_news:
+        for item in nasdaq_news:
             # Get rid of data without ticker symbol
-            if news[1] != "":
+            if item[1] != "":
 
                 #Change date to proper format without T and Z
-                news[0] = list(news[0])
-                news[0] = [w.replace("T", " ") for w in news[0]]
-                news[0] = [w.replace("Z", " ") for w in news[0]]
-                news[0] = "".join(news[0])
-                news[0] = news[0].split(" ")[0]
+                item[0] = list(item[0])
+                item[0] = [w.replace("T", " ") for w in item[0]]
+                item[0] = [w.replace("Z", " ") for w in item[0]]
+                item[0] = "".join(item[0])
+                item[0] = item[0].split(" ")[0]
 
                 #If multiple tickers use first entry. IMPORTANT: adjust later so that file multiple tickers same text
-                news[1] = news[1].split(sep= ",")[0]
+                item[1] = item[1].split(sep= ",")[0]
 
                 #Pre-processing text to reduce noise and prepare text data for word representation techniques (word embedding). Part of the code is taken from Frankel, Jennings and Lee (2021)
-                news[2] = fix_phrases(news[2])
-                sentences = news[2].split('.')
+                item[2] = fix_phrases(item[2])
+                sentences = item[2].split('.')
 
                 for v,sentence in enumerate(sentences):
 
@@ -116,24 +116,29 @@ with open(first_csv_path, newline='') as first_csv:
                         allwords[w] = fixword(allwords[w])
 
                     sentences[v] = " ".join(allwords)
-                    print(sentences[v])
 
-                news[2] = ".".join(sentences)
 
-                writer.writerow([news[0], news[1], news[2]])
+                item[2] = ".".join(sentences)
+
+                writer.writerow([item[0], item[1], item[2]])
 
         first_csv.close()
         csv_processed.close()
 
-#Open newly created csv file in read mode which contains tickers
-df = pd.read_csv(csv_processed_path)
+os.remove(output_file_path + "data_processed_interim.csv")
 
-txtticker_path = "/Users/alexanderholzer/PycharmProjects/Thesis/Data/processed data/data_compact_ticker.txt"
+#Open newly created csv file in read mode which contains tickers
+df = pd.read_csv(output_file_path + "data_processed.csv")
+
+txtticker_path = "/Users/alexanderholzer/PycharmProjects/Thesis/Data/processed data/"
 
 #Write txt file as file document supported on wrds database with txt ticker names to get permno to upload on wrds to get permnos as csv (select only latest permnos):https://wrds-www.wharton.upenn.edu/pages/get-data/center-research-security-prices-crsp/annual-update/tools/translate-to-permcopermno/
-with open(txtticker_path, "w") as f:
+with open(txtticker_path + "dataticker.txt", "w") as f:
     for index, row in df.iterrows():
         f.write(str(row["Ticker"]) + "\n")
+
+
+
 
 
 
